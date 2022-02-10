10d36bf3a6c99cc901e052292e180ee76e6cd37f
evgeny.ravlyuk <yevhenii.ravliuk@jetsoftpro.com>
2022-02-10 15:29:55 +0200

    Add round_time

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ logger.remove(0)
 logger.add(sys.stderr, format="<blue>{time:YYYY-MM-DD HH:mm:ss}</blue> <green>{message}</green>", colorize=True)
 
 
+def round_time(long_number: float) -> float:
+    return round(long_number, 2)
+
+
 def test_speed(func, name: str, num: int, repeat: int) -> float:
     start = time.time()
     result: int = func(num, repeat)
     finish = time.time()
-    spent_time = round(finish - start, 2)
-    logger.debug(f'Час виконання {name}: {spent_time}, результат: {result}')
+    spent_time = finish - start
+    logger.debug(f'Час виконання {name}: {round_time(spent_time)}, результат: {result}')
     return spent_time
 
 
@@ -26,7 +30,7 @@ def main(times: int = 5):
         speed_python = test_speed(name='Python', func=fibonacci_python.test, **params)
         speed_cython = test_speed(name='Cython', func=fibonacci_cython.test, **params)
         delta = int(speed_python / speed_cython)
-        logger.debug(f'Cython швидший в {delta} раз')
+        logger.debug(f'Cython швидший в {round_time(delta)} раз')
         deltas.append(delta)
     avg_speed = int(sum(deltas) / len(deltas))
     logger.debug(f'Тестування завершено! В середньому Cython швидший в {avg_speed} раз!')
